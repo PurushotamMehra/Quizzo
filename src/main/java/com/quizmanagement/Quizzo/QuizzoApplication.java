0c66984bf3cdbd2e207789cc60ba2cca95408a13
PurushotamMehra <purushotam.mehra2001@gmail.com>
2025-02-16 17:33:53 +0530

    First commit with everything hopefully

---
+++ src/main/java/com/quizmanagement/Quizzo/QuizzoApplication.java
@@ -0,0 +1,13 @@
+package com.quizmanagement.Quizzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quizmanagement/Quizzo/QuizzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizmanagement.Quizzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
